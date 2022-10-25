a0c8568b149f324f71007f8c682471f8d943d635
황영현 <53035183+hyh528@users.noreply.github.com>
2022-10-25 11:54:18 +0900

    function
    함수

---
+++ python_function.py
@@ -0,0 +1,39 @@
+# def name(input) :
+#     print('제 이름은 ' + input +'입니다.')
+
+# name("영현")
+
+# def sum(a,b) :
+#      print(a+b)
+#      return a+b
+     
+    
+# total = sum(3,4)
+
+# total
+# result = ""
+# def grade(score, name) :
+#     if score >= 90 :
+#         print(name + '의 등급은 A입니다.')
+#     elif score >= 80 :
+#         print(name + '의 등급은 B입니다.')
+#     elif score >= 70 :
+#         print(name + '의 등급은 C입니다.')
+#     elif score >= 60 :
+#         print(name + '의 등급은 D입니다.')
+#     else :
+#         print("낙제")
+
+# result = grade(90, '영현')
+
+
+import random
+# b = random.random()
+# print(b)
+
+# a = [1,2,3]
+# random.shuffle(a)
+
+import sys
+print(sys.argv)
+print(sys.version)
